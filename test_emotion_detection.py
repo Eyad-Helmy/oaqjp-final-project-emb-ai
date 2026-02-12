321658aa0d5adde0fdb7dd1b0b17ccb7c34da203
Eyad-Helmy <meyad8375@gmail.com>
2026-02-12 16:45:50 -0500

    unit tests

---
+++ test_emotion_detection.py
@@ -0,0 +1,25 @@
+import unittest
+from EmotionDetection.emotion_detection import emotion_detector
+
+class TestEmotionDetection(unittest.TestCase):
+    def test_joy(self):
+        result = emotion_detector("I am glad this happened")
+        self.assertEqual(result, "joy")
+        
+    def test_anger(self):
+        result = emotion_detector("I am really mad about this")
+        self.assertEqual(result, "anger")
+
+    def test_disgust(self):
+        result = emotion_detector("I feel disgusted just hearing about this")
+        self.assertEqual(result, "disgust")
+
+    def test_sadness(self):
+        result = emotion_detector("I am so sad about this")
+        self.assertEqual(result, "sadness")
+
+    def test_fear(self):
+        result = emotion_detector("I am really afraid that this will happen")
+        self.assertEqual(result, "fear")
+
+unittest.main()
